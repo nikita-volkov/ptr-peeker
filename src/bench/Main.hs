ebb58f7b22ad0716016cc1bc399b42981aeb2dfe
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-10-15 11:17:39 +0300

    Enable bug location deteciton

--- src/bench/Main.hs
+++ src/bench/Main.hs
@@ -2,6 +2,7 @@ module Main (main) where
 
 import Criterion.Main
 import Data.Serialize qualified as Cereal
+import GHC.Stack (HasCallStack)
 import Data.Store qualified as Store
 import Data.Vector qualified as V
 import Data.Vector.Unboxed qualified as Vu
@@ -96,7 +97,7 @@ main = do
   defaultMain groups
 
 -- | Test functions and create a benchmark group out of them.
-initGroup :: (Eq a, Show a, NFData a) => String -> ByteString -> a -> [(String, ByteString -> Maybe a)] -> IO Benchmark
+initGroup :: HasCallStack => (Eq a, Show a, NFData a) => String -> ByteString -> a -> [(String, ByteString -> Maybe a)] -> IO Benchmark
 initGroup name input correctDecoding subjects = do
   fmap (bgroup name) . forM subjects $ \(name, f) -> do
     Tasty.assertEqual name (Just correctDecoding) (f input)
